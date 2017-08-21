1e17803b74d92e1a9f3452d8eb0a28b5b58bd2d6
Kara de la Marck <karadelamarck@gmail.com>
2017-08-21 10:00:53 +0100

    addition of modified babel transform plugin variable

--- packages/babel-plugin-transform-codemod-var/src/index.js
+++ packages/babel-plugin-transform-codemod-var/src/index.js
@@ -1,23 +1,64 @@
 module.exports = function() {
-  const isRightHandSideAnIndentifier = path => {
+  const isRightHandSideAnIdentifier = path => {
     return path.node.declarations[0].init.type === "Identifier";
   };
   const shouldItBeAConstant = path => {
-    if (isRightHandSideAnIndentifier(path)) {
+    if (isRightHandSideAnIdentifier(path)) {
       return false;
     }
     return true;
   };
+
+  const varDeclaratorArray = [];
+
+  const getDeclaratorNames = path =>
+    path.scope.block.body[0].declarations[0].id.name;
+
+  //getLocalScope returns array of Var Names in local scope (no duplicates)
+  const getLocalScope = (scope, parentScope) => {
+    const names = [];
+    while (scope !== parentScope) {
+      if (Array.isArray(scope.block.body)) {
+        scope.value.body.forEach(node => {
+          if (node.type === "VariableDeclaration") {
+            node.declarations.map(getDeclaratorNames).forEach(dNames => {
+              dNames.forEach(name => {
+                if (names.indexOf(name) === -1) {
+                  names.push(name);
+                }
+              });
+            });
+          }
+        });
+      }
+    }
+  };
   return {
     visitor: {
       VariableDeclaration(path) {
+        console.log("scope", path.scope);
         if (path.node.kind == "var") {
-          if (shouldItBeAConstant(path)) {
+          const nameofVar = path.node.declarations[0].id.name;
+          console.log("nameofVar", nameofVar);
+          varDeclaratorArray.push(nameofVar);
+          console.log("varDeclaratorArray", varDeclaratorArray);
+          const numTimesAssigned = varDeclaratorArray.filter(name => {
+            return name === nameofVar;
+          }).length;
+
+          console.log("numTimesAssigned", numTimesAssigned);
+          console.log(numTimesAssigned);
+          if (shouldItBeAConstant(path) || numTimesAssigned <= 1) {
             path.node.kind = "const";
           } else {
             path.node.kind = "let";
           }
         }
+        const localScopeNames = getLocalScope(
+          path.scope,
+          path.scope.parentBlock,
+        );
+        console.log(localScopeNames);
       },
     },
   };
