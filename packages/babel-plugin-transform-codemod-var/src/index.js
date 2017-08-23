8bb92267eb749ac0e79b36ed62d7ce7ab48ad8d6
Kara de la Marck <karadelamarck@gmail.com>
2017-08-23 10:43:50 +0100

    Cutting out dead wood of our trials, 4 tests still passing, new test on reassigning var still failing

--- packages/babel-plugin-transform-codemod-var/src/index.js
+++ packages/babel-plugin-transform-codemod-var/src/index.js
@@ -9,60 +9,17 @@ module.exports = function() {
     return true;
   };
 
-  const varDeclaratorArray = [];
-
-  // const getDeclaratorNames = path =>
-  //   path.scope.block.body[0].declarations[0].id.name;
-
-  //getLocalScope returns array of Var Names in local scope (no duplicates)
-  const getLocalScope = (scope, parentScope) => {
-    const names = [];
-    if (scope !== parentScope) {
-      if (Array.isArray(scope.block.body)) {
-        scope.block.body.forEach(node => {
-          if (node.type === "VariableDeclaration") {
-            // node.declarations.map(getDeclaratorNames).forEach(dNames => {
-            //   dNames.forEach(name => {
-            //     if (names.indexOf(name) === -1) {
-            //       names.push(name);
-            //     }
-            //   });
-            // });
-            node.declarations.map(declaration => {
-              return names.push(declaration.name);
-            });
-          }
-        });
-      }
-    }
-    console.log("names", names);
-    return names;
-  };
   return {
     visitor: {
       VariableDeclaration(path) {
-        //console.log("scope", path.scope);
         if (path.node.kind == "var") {
-          const nameofVar = path.node.declarations[0].id.name;
-          //console.log("nameofVar", nameofVar);
-          varDeclaratorArray.push(nameofVar);
-          //console.log("varDeclaratorArray", varDeclaratorArray);
-          const numTimesAssigned = varDeclaratorArray.filter(name => {
-            return name === nameofVar;
-          }).length;
-
-          console.log("numTimesAssigned", numTimesAssigned);
-          if (shouldItBeAConstant(path) || numTimesAssigned <= 1) {
+          //const nameofVar = path.node.declarations[0].id.name;
+          if (shouldItBeAConstant(path)) {
             path.node.kind = "const";
           } else {
             path.node.kind = "let";
           }
         }
-        const localScopeNames = getLocalScope(
-          path.scope,
-          path.scope.parentBlock,
-        );
-        console.log(localScopeNames);
       },
     },
   };
