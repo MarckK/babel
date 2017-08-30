d4504d2a2f84ee3fff7552a6ab548e24d83efed7
Kara de la Marck <karadelamarck@gmail.com>
2017-08-30 15:45:15 +0100

    Refactor visitor in index.js to convert var to let when have mutiple assignments to variable

--- packages/babel-plugin-transform-codemod-var/src/index.js
+++ packages/babel-plugin-transform-codemod-var/src/index.js
@@ -2,23 +2,19 @@ module.exports = function() {
   const isRightHandSideAnIdentifier = path => {
     return path.node.declarations[0].init.type === "Identifier";
   };
-  const shouldItBeAConstant = path => {
-    if (isRightHandSideAnIdentifier(path)) {
-      return false;
-    }
-    return true;
-  };
 
   return {
     visitor: {
       VariableDeclaration(path) {
-        if (path.node.kind == "var") {
-          //const nameofVar = path.node.declarations[0].id.name;
-          if (shouldItBeAConstant(path)) {
-            path.node.kind = "const";
-          } else {
-            path.node.kind = "let";
-          }
+        const varName = path.node.declarations[0].id.name;
+        const binding = path.scope.getOwnBinding(varName);
+        if (
+          binding.constantViolations.length === 0 &&
+          !isRightHandSideAnIdentifier(path)
+        ) {
+          path.node.kind = "const";
+        } else {
+          path.node.kind = "let";
         }
       },
     },
